26f8a7cf2858201b14dc326c7ec025aab655a7e2
masterxyeti <97883620+masterxyeti@users.noreply.github.com>
2022-02-01 15:03:54 +0100

    Update programm.cs

--- programm.cs
+++ programm.cs
@@ -30,19 +30,46 @@ namespace AB6_Aufgabe1
 
             Console.WriteLine();
 
-            for(j = 5; j <=13; j += 2)
+            for (j = 5; j <= 13; j += 2)
             {
                 Console.Write("Z" + j + " ");
             }
             Console.WriteLine();
 
-            for(i = 1; i <= 3; i++)
+            for (i = 1; i <= 3; i++)
             {
                 Console.Write("ab" + i + " ");
             }
 
             Console.WriteLine();
 
+            for (i = 2; i <= 3; i++)
+            {
+                Console.Write("c" + i + " ");
+            }
+
+            for (i = 12; i <= 13; i++)
+            {
+                Console.Write("c" + i + " ");
+            }
+
+            for (i = 22; i <= 23; i++)
+            {
+                Console.Write("c" + i + " ");
+            }
+
+            Console.WriteLine();
+
+            for (i = 13; i <= 45; i+=4)
+            {
+                Console.Write(i + " ");
+                if (i == 21)
+                {
+                    i += 8;
+                }
+            }
+
+            Console.WriteLine();
 
             Console.ReadKey();
         }
